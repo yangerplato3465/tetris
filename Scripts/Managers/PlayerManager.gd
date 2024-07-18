22bc1986cea95bb4dbf98bd0bca108547c01bd2c
Chowdog <yangerplato3465@gmail.com>
2024-07-18 22:31:07 +0800

    assets

---
+++ Scripts/Managers/PlayerManager.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var visibleNextPiece = 1
+var singleDamage = 10
+var doubleDamage = 30
+var tripleDamage = 50
+var tetrisDamage = 80
+var damageMult = 1
+var comboMult = 1.2
+
+func _ready():
+	pass
+
+func reset(): 
+	visibleNextPiece = 1
+	singleDamage = 10
+	doubleDamage = 30
+	tripleDamage = 50
+	tetrisDamage = 80
+	damageMult = 1
+	comboMult = 1.2
+
--- Scripts/NextPieces.gd
+++ Scripts/NextPieces.gd
@@ -1,11 +1,10 @@
 extends PiecePanel
 
-const numberOfPieces = 5
 const separation = 105
 
 func drawPieces(currentBag, nextBag):
 	Utilities.delete_children(self)
 	var fullQueue = currentBag.duplicate()
 	fullQueue.append_array(nextBag)
-	for i in range(numberOfPieces):
+	for i in range(PlayerManager.visibleNextPiece):
 		drawPiece(fullQueue[i], i*separation)
