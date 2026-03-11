e134d9e43f4f4378c5dbfff72cc9ff7b7d7963b7
Patrick <patrick0885@cchntek.com>
2026-03-11 23:56:31 +0800

    Optimize utility code

--- Scripts/Utils/Utilities.gd
+++ Scripts/Utils/Utilities.gd
@@ -2,145 +2,102 @@ extends Node
 
 func delete_children(node):
 	for n in node.get_children():
-		if n.is_class("Sprite2D"):
-			node.remove_child(n)
+		if n is Sprite2D:
 			n.queue_free()
+
+# --- Tween helpers ---
+
+func _tween_scale(node, target: Vector2, duration: float = 0.5):
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_ELASTIC)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(node, "scale", target, duration)
 
 func scaleUp(node):
 	AudioManager.hover.play()
-	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_ELASTIC) 
-	tween.set_ease(Tween.EASE_OUT)
-	tween.tween_property(node, "scale", Vector2(1.05, 1.05), 0.5)
+	_tween_scale(node, Vector2(1.05, 1.05))
 
 func scaleDown(node):
-	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_ELASTIC) 
-	tween.set_ease(Tween.EASE_OUT)
-	tween.tween_property(node, "scale", Vector2(1, 1), 0.5)
+	_tween_scale(node, Vector2(1, 1))
 
 func onPressed(node):
 	AudioManager.button_press.play()
-	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_ELASTIC) 
-	tween.set_ease(Tween.EASE_OUT)
-	tween.tween_property(node, "scale", Vector2(0.9, 0.9), 0.5)
+	_tween_scale(node, Vector2(0.9, 0.9))
 
 func showPanel(node):
 	node.scale = Vector2(0.5, 0.5)
 	node.visible = true
-	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_ELASTIC) 
-	tween.set_ease(Tween.EASE_OUT)
-	tween.tween_property(node, "scale", Vector2(1, 1), 0.7)
+	_tween_scale(node, Vector2(1, 1), 0.7)
 
 func hidePanel(node):
 	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_BACK) 
+	tween.set_trans(Tween.TRANS_BACK)
 	tween.set_ease(Tween.EASE_IN)
 	tween.tween_property(node, "scale", Vector2(0.5, 0.5), 0.3)
-	tween.finished.connect(func():
-		node.visible = false
-	)
+	tween.finished.connect(func(): node.visible = false)
 
 func slideOut(outNode):
 	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_BACK) 
+	tween.set_trans(Tween.TRANS_BACK)
 	tween.set_ease(Tween.EASE_IN)
 	tween.tween_property(outNode, "position:y", outNode.position.y + 500, 0.5)
-	tween.finished.connect(func():
-		outNode.visible = false
-		# 重置位置，避免下次 slideIn 時位置錯誤
-		outNode.position.y = 0
-	)
+	await tween.finished
+	outNode.visible = false
+	outNode.position.y = 0
 
 func slideIn(node, callback: Callable = Callable()):
 	node.visible = true
 	node.position.y = 900
 	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_QUINT) 
+	tween.set_trans(Tween.TRANS_QUINT)
 	tween.set_ease(Tween.EASE_OUT)
 	tween.tween_property(node, "position:y", 0, 1).set_delay(1)
 	if callback.is_valid():
-		tween.finished.connect(func():
-			callback.call()
-		)
+		tween.finished.connect(callback)
 
-func chooseRandom(arraySize: int, size: int):
-	var indices = []
-	
-	while indices.size() < size:
-		var index = randi() % arraySize
-		if index not in indices:
-			indices.append(index)
-	
-	return indices
+# --- Board generation ---
+
+func _generateMessyBoard(filledRows: int):
+	var messyGrid = []
+	for _x in range(10):
+		var col = []
+		for _y in range(23 - filledRows):
+			col.append(0)
+		for _y in range(filledRows):
+			col.append(randomNum())
+		messyGrid.append(col)
+	return messyGrid
+
+func generateSmallMessyBoard():
+	return _generateMessyBoard(2)
+
+func generateMediumMessyBoard():
+	return _generateMessyBoard(4)
+
+func generateLargeMessyBoard():
+	return _generateMessyBoard(6)
+
+func randomNum():
+	return randi_range(0, 1)
+
+# --- Misc ---
+
+func chooseRandom(arraySize: int, size: int) -> Array:
+	var indices = range(arraySize)
+	indices.shuffle()
+	return indices.slice(0, size)
 
 func shakeNode(node, originalPos):
 	var tween = create_tween()
 	var shake = 5
 	var shake_duration = 0.05
 	var shake_count = 20
-	var originalPosition = node.position
-	tween.finished.connect(func():
-		node.position = originalPos
-	)
-
+	tween.finished.connect(func(): node.position = originalPos)
 	for i in shake_count:
-		tween.tween_property(node, "position", Vector2(originalPosition.x + randf_range(-shake, shake), originalPosition.y + randf_range(-shake, shake)), 
-		shake_duration)
-
-func generateSmallMessyBoard():
-	var messyGrid = [
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,randomNum(),randomNum()],
-	]
-
-	return messyGrid
-
-func generateMediumMessyBoard():
-	var messyGrid = [
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,randomNum(),  randomNum(),randomNum(),randomNum()],
-	]
-
-	return messyGrid
-
-func generateLargeMessyBoard():
-	var messyGrid = [
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,randomNum(),randomNum(),randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,randomNum(),randomNum(),randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,randomNum(),randomNum(),randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,randomNum(),randomNum(),randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,randomNum(),randomNum(),randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,randomNum(),randomNum(),randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,randomNum(),randomNum(),randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,randomNum(),randomNum(),randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,randomNum(),randomNum(),randomNum(),  randomNum(),randomNum(),randomNum()],
-		[0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,randomNum(),randomNum(),randomNum(),  randomNum(),randomNum(),randomNum()],
-	]
-
-	return messyGrid
-
-func randomNum():
-	var num = randi_range(0, 1)
-	return num
+		tween.tween_property(node, "position", Vector2(
+			originalPos.x + randf_range(-shake, shake),
+			originalPos.y + randf_range(-shake, shake)
+		), shake_duration)
 
 func floorNum(number):
 	return floor(number * 10) / 10.0
