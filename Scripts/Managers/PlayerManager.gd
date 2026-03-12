7ce89a6826aea9196ebed4120d228635fffd0358
Patrick <patrick0885@cchntek.com>
2026-03-12 12:20:31 +0800

    Refactor PlayerManager upgrade function

--- Scripts/Managers/PlayerManager.gd
+++ Scripts/Managers/PlayerManager.gd
@@ -48,10 +48,13 @@ var linesCleared
 var highestCombo
 var totalDamageDealt
 
-func _ready():
-	_set_defaults()
+var _upgrade_effects: Dictionary = {}
 
-func _set_defaults():
+func _ready():
+	_setDefaults()
+	_initUpgradeEffects()
+
+func _setDefaults():
 	visibleNextPiece = 1
 	canHoldPiece = false
 	singleDamage = 10
@@ -80,85 +83,66 @@ func _set_defaults():
 
 func reset():
 	currentLevel = 1
-	_set_defaults()
+	_setDefaults()
 
-func applyUpgrades(id, price):
+func _initUpgradeEffects():
+	_upgrade_effects = {
+		0:  func(): singleDamage += 20,
+		1:  func(): doubleDamage += 20,
+		2:  func(): tripleDamage += 20,
+		3:  func(): tetrisDamage += 20,
+		4:  func(): comboMult += 0.1,
+		5:  func(): timer += 10,
+		6:  func(): singleDamage += 40,
+		7:  func(): doubleDamage += 40,
+		8:  func(): tripleDamage += 40,
+		9:  func(): tetrisDamage += 40,
+		10: func(): timer += 15,
+		11: func(): spawnBag.append_array([0, 0]),
+		12: func(): comboMult += 0.1,
+		13: func(): _applyAllDamage(30),
+		14: func(): singleDamage *= 2,
+		15: func(): doubleDamage *= 2,
+		16: func(): tripleDamage *= 2,
+		17: func(): tetrisDamage *= 2,
+		18: func(): comboMult += 0.5,
+		19: func(): _applyUnlockHold(),
+		20: func(): _applyUnlockNext(20),
+		21: func(): _applyUnlockNext(21),
+		22: func(): _applyUnlockNext(22),
+		23: func(): _applyTierUnlock(1, 23),
+		24: func(): hardDropDamage = true; removeEquipment(24),
+		25: func(): treasureBox = true; removeEquipment(25),
+		26: func(): _applyTierUnlock(2, 26),
+		27: func(): ocarina = true; removeEquipment(27),
+	}
+
+func applyUpgrades(id: int, price: int):
 	coin -= price
-	match id:
-		0: # Alchemy
-			singleDamage += 20
-		1:
-			doubleDamage += 20
-		2:
-			tripleDamage += 20
-		3:
-			tetrisDamage += 20
-		4:
-			comboMult += 0.1
-		5:
-			timer += 10
-		6:
-			singleDamage += 40
-		7:
-			doubleDamage += 40
-		8:
-			tripleDamage += 40
-		9:
-			tetrisDamage += 40
-		10:
-			timer += 15
-		11:
-			spawnBag.append_array([0, 0])
-		12:
-			comboMult += 0.1
-		13:
-			singleDamage += 30
-			doubleDamage += 30
-			tripleDamage += 30
-			tetrisDamage += 30
-		14:
-			singleDamage *= 2
-		15:
-			doubleDamage *= 2
-		16:
-			tripleDamage *= 2
-		17:
-			tetrisDamage *= 2
-		18:
-			comboMult += 0.5
-		19: # Equipments
-			canHoldPiece = true
-			unlockHold.emit(false)
-			removeEquipment(19)
-		20:
-			visibleNextPiece += 1
-			unlockNextPiece.emit()
-			removeEquipment(20)
-		21:
-			visibleNextPiece += 1
-			unlockNextPiece.emit()
-			removeEquipment(21)
-		22:
-			visibleNextPiece += 1
-			unlockNextPiece.emit()
-			removeEquipment(22)
-		23:
-			updateItemArrays(1)
-			removeEquipment(23)
-		24:
-			hardDropDamage = true
-			removeEquipment(24)
-		25:
-			treasureBox = true
-			removeEquipment(25)
-		26:
-			updateItemArrays(2)
-			removeEquipment(26)
-		27:
-			ocarina = true
-			removeEquipment(27)
+	if _upgrade_effects.has(id):
+		_upgrade_effects[id].call()
 
-func updateItemArrays(tier):
+func _applyAllDamage(amount: int):
+	singleDamage += amount
+	doubleDamage += amount
+	tripleDamage += amount
+	tetrisDamage += amount
+
+func _applyUnlockHold():
+	canHoldPiece = true
+	unlockHold.emit(false)
+	removeEquipment(Consts.UpgradeID.OLD_KEY)
+
+func _applyUnlockNext(equipment_id: int):
+	visibleNextPiece += 1
+	unlockNextPiece.emit()
+	removeEquipment(equipment_id)
+
+func _applyTierUnlock(tier: int, equipment_id: int):
+	_updateItemArrays(tier)
+	removeEquipment(equipment_id)
+
+func _updateItemArrays(tier: int):
 	match tier:
 		1:
 			alchemyArray.append_array(Consts.alchemyRareItems)
@@ -167,7 +151,7 @@ func updateItemArrays(tier):
 			alchemyArray.append_array(Consts.alchemyLegendaryItems)
 			equipmentArray.append_array(Consts.equipmentLegendaryItems)
 
-func removeEquipment(id):
+func removeEquipment(id: int):
 	for index in range(equipmentArray.size()):
 		if equipmentArray[index]["id"] == id:
 			equipmentArray.remove_at(index)
